8bb84bd39d3ab2139eeaa905fbdbf66f82cd43b0
Ville Jantunen <sudoku92@gmail.com>
2021-04-25 23:00:35 +0300

    intenseburn has an animation

--- Scripts/Objects/LightSource.gd
+++ Scripts/Objects/LightSource.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var light_on = true
 var intense_light = false
+var end_tween_cycle = false
 
 func toggle_light():
 	light_on = !light_on
@@ -15,15 +16,25 @@ func toggle_intense_light():
 	if(!intense_light and light_on):
 		$LightHitBox.collision_mask = 32
 		intense_light = true
+		end_tween_cycle = false
+		$Tween.interpolate_property($Light2D,"energy",1.0, 2.0, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween.start()
 	else:
 		intense_light = false
 		if(light_on):
 			$LightHitBox.collision_mask = 1
 		else:
 			$LightHitBox.collision_mask = 8
-func _ready():
-	pass # Replace with function body.
 
+func intense_burn_animation():
+	if(!end_tween_cycle):
+		$Tween.interpolate_property($Light2D,"energy",2.0, 1.0, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween.start()
+		end_tween_cycle = true
+
+func _ready():
+	$Tween.connect("tween_all_completed", self, "intense_burn_animation")
+	print($Light2D.energy)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
